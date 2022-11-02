fe1c50a3e9b2be8a12ad91b874129b9d6332e76b
Rodrigo Paiva <63483901+paiva-rodrigo@users.noreply.github.com>
2022-11-02 12:29:49 -0300

    secound commit

---
+++ Endpoints/Categories/CategoryPost.cs
@@ -0,0 +1,16 @@
+﻿using IWantApp.Infra.Data;
+
+namespace IWantApp.Endpoints.Categories;
+
+public class CategoryPost
+{
+    public static string Template => "/categories";
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+        return Results.Ok("OK");
+    }
+
+}
